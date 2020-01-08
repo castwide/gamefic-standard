2f8d94a201aa8588b2de64c51082e5d53decf0da
Fred Snyder <fsnyder@castwide.com>
2020-01-08 11:03:43 -0500

    Look room spec.

--- spec/actions/look_spec.rb
+++ spec/actions/look_spec.rb
@@ -18,6 +18,7 @@ RSpec.describe 'Look action' do
     plot = Gamefic::Plot.new
     plot.stage do
       room = make Room, name: 'room', description: 'your area'
+      thing = make Thing, name: 'thing', description: 'a thing', parent: room
       introduction do |actor|
         actor.parent = room
       end
@@ -26,5 +27,14 @@ RSpec.describe 'Look action' do
     plot.introduce actor
     actor.perform "look"
     expect(actor.messages).to include(plot.pick('room').description)
+    expect(actor.messages).to include('thing')
   end
+
+  # it 'looks at itself' do
+  #   plot = Gamefic::Plot.new
+  #   actor = plot.get_player_character
+  #   actor.description = 'my description'
+  #   actor.perform "look self"
+  #   expect(actor.messages).to include(actor.description)
+  # end
 end
