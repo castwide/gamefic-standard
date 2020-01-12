c0603022ca6b635b6782d46e9d13c8beb663d0a6
Fred Snyder <fsnyder@castwide.com>
2020-01-12 02:43:01 -0500

    Insert spec for closed containers.

--- spec/actions/insert_spec.rb
+++ spec/actions/insert_spec.rb
@@ -36,4 +36,22 @@ RSpec.describe 'insert action' do
     actor.perform 'insert thing container'
     expect(plot.entities[1].parent).to eq(plot.entities[2])
   end
+
+  it 'does not insert a child in a closed container' do
+    plot = Gamefic::Plot.new
+    plot.stage do
+      room = make Room, name: 'room'
+      thing = make Thing, name: 'thing'
+      container = make Container, name: 'container', parent: room, open: false
+
+      introduction do |actor|
+        actor.parent = room
+        thing.parent = actor
+      end
+    end
+    actor = plot.get_player_character
+    plot.introduce actor
+    actor.perform 'insert thing container'
+    expect(plot.entities[1].parent).not_to eq(plot.entities[2])
+  end
 end
