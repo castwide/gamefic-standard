2b298abfeb158ef691265bb49620b6b3ee684955
Fred Snyder <fsnyder@castwide.com>
2020-03-18 21:54:36 -0400

    Grammar::Attributes documentation.

--- lib/gamefic-standard/grammar/attributes.rb
+++ lib/gamefic-standard/grammar/attributes.rb
@@ -6,21 +6,30 @@ module Grammar
     # @see #gender
     attr_writer :gender
 
+    # @see #plural?
+    attr_writer :plural
+
     # The gender of the object. Supported values are :male, :female, :neutral,
     # and :other. Use :neutral for objects that don't have a gender (i.e.,
     # "it"). Use :other for people or characters that have an unspecified or
     # non-binary gender (i.e., "they").
+    #
     # @return [Symbol]
     def gender
       @gender ||= :neutral
     end
 
-    # @todo Make it more expressive
+    # True if the object should be referred to in the plural, e.g., "they"
+    # instead of "it."
+    # @return [Boolean]
+    #
     def plural?
-      false
+      @plural ||= false
     end
 
-    # @todo Make it more expressive
+    # For now, the object's person is always assumed to be third
+    # (he/she/it/they). A future version of this library might support first
+    # (I/me) and second (you).
     def person
       3
     end
