d49ee8369372db7294da1dc57e7e6e54c9355954
Fred Snyder <fsnyder@castwide.com>
2020-10-05 16:01:55 -0400

    Additional token error handling.

--- lib/gamefic-standard/actions/nil.rb
+++ lib/gamefic-standard/actions/nil.rb
@@ -1,5 +1,5 @@
 Gamefic.script do
-  meta nil, Gamefic::Query::Text.new() do |actor, string|
+  meta nil, Gamefic::Query::Text.new do |actor, string|
     words = string.split_words
     list = verbs
     if list.include?(words[0])
@@ -8,7 +8,7 @@ Gamefic.script do
         if found.empty?
           actor.tell %(I recognize "#{words[0]}" as a verb but could not understand the rest of your sentence.)
         elsif found.one?
-          actor.tell %(I recognize "#{words[0]}" and "#{the found.first}" but could not understand it in this context.)
+          actor.tell %(I recognize "#{words[0]}" and "#{found.first.name}" but could not understand them together.)
         else
           actor.tell %(I'm not sure if "#{words[1..-1].join(' ')}" means #{found.map(&:definitely).join_or}.)
         end
--- spec/actions/nil_spec.rb
+++ spec/actions/nil_spec.rb
@@ -27,8 +27,8 @@ RSpec.describe 'Nil action' do
   it 'reports unrecognized tokens' do
     plot = Gamefic::Plot.new
     room = plot.make Room
-    item1 = plot.make Item, name: 'item 1', parent: room
-    item2 = plot.make Item, name: 'item 2', parent: room
+    plot.make Item, name: 'item 1', parent: room
+    plot.make Item, name: 'item 2', parent: room
     plot.respond :foobar, Item do |actor, item|
       item.parent = actor
     end
@@ -37,6 +37,40 @@ RSpec.describe 'Nil action' do
     actor.parent = room
     actor.perform 'foobar nothing'
     expect(actor.children).to be_empty
-    expect(actor.messages).to include("could not understand the rest")
+    expect(actor.messages).to include('could not understand the rest')
+  end
+
+  it 'reports missing tokens' do
+    plot = Gamefic::Plot.new
+    room = plot.make Room
+    plot.make Item, name: 'item 1', parent: room
+    plot.make Fixture, name: 'fixture', parent: room
+    plot.respond :foobar, Item do |actor, item|
+      item.parent = actor
+    end
+    actor = plot.get_player_character
+    plot.introduce actor
+    actor.parent = room
+    actor.perform 'foobar'
+    expect(actor.children).to be_empty
+    expect(actor.messages).to include('foobar')
+    expect(actor.messages).to include('could not understand')
+  end
+
+  it 'reports unhandled tokens' do
+    plot = Gamefic::Plot.new
+    room = plot.make Room
+    plot.make Item, name: 'item 1', parent: room
+    plot.make Fixture, name: 'fixture', parent: room
+    plot.respond :foobar, Item do |actor, item|
+      item.parent = actor
+    end
+    actor = plot.get_player_character
+    plot.introduce actor
+    actor.parent = room
+    actor.perform 'foobar fixture'
+    expect(actor.children).to be_empty
+    expect(actor.messages).to include('foobar')
+    expect(actor.messages).to include('fixture')
   end
 end
