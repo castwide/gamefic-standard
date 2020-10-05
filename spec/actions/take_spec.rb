df3f7abc060b2b4dae909474b345063866c22424
Fred Snyder <fsnyder@castwide.com>
2020-10-05 15:11:54 -0400

    Take action with receptacles.

--- spec/actions/take_spec.rb
+++ spec/actions/take_spec.rb
@@ -13,4 +13,36 @@ RSpec.describe 'Take action' do
     actor.perform 'take item'
     expect(actor.children.first.name).to eq('item')
   end
+
+  it 'takes items from receptacles implicitly' do
+    plot = Gamefic::Plot.new
+    plot.stage do
+      room = make Room
+      receptacle = make Receptacle, name: 'receptacle', parent: room
+      make Item, name: 'item', parent: receptacle
+      introduction do |actor|
+        actor.parent = room
+      end
+    end
+    actor = plot.get_player_character
+    plot.introduce actor
+    actor.perform 'take item'
+    expect(actor.children.first.name).to eq('item')
+  end
+
+  it 'takes items from receptacles explicitly' do
+    plot = Gamefic::Plot.new
+    plot.stage do
+      room = make Room
+      receptacle = make Receptacle, name: 'receptacle', parent: room
+      make Item, name: 'item', parent: receptacle
+      introduction do |actor|
+        actor.parent = room
+      end
+    end
+    actor = plot.get_player_character
+    plot.introduce actor
+    actor.perform 'take item from receptacle'
+    expect(actor.children.first.name).to eq('item')
+  end
 end
