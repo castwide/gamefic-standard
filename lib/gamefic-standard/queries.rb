a18f7fcde14cfceeed5add371148352d8185f27d
Fred Snyder <fsnyder@castwide.com>
2023-12-05 17:56:10 -0500

    Remove Scope::Room precision

--- lib/gamefic-standard/queries.rb
+++ lib/gamefic-standard/queries.rb
@@ -2,14 +2,10 @@ class Gamefic::Scope::Room < Gamefic::Scope::Base
   def matches
     [context.room].compact
   end
-
-  def self.precision
-    1000
-  end
 end
 
 module Gamefic::Delegatable::Queries
   def room *args
-    Gamefic::Query::Scoped.new Gamefic::Scope::Room, *args
+    Gamefic::Query::Scoped.new Gamefic::Scope::Room, *([Room] + args)
   end
 end
