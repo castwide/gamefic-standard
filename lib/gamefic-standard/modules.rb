91ced16d220795f2399e6c17a05019e9494ed8b0
Fred Snyder <fsnyder@castwide.com>
2024-09-25 20:25:44 -0400

    Unused modules script

--- lib/gamefic-standard/modules.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-require 'gamefic-standard/modules/standardized'
-require 'gamefic-standard/modules/enterable'
-require 'gamefic-standard/modules/openable'
-require 'gamefic-standard/modules/lockable'
