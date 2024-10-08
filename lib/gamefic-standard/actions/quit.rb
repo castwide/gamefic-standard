3267d1bbda56b0e114e6f34ff0c49b46c311015a
Fred Snyder <fsnyder@castwide.com>
2024-10-08 07:40:40 -0400

    ConfirmQuit clip

--- lib/gamefic-standard/actions/quit.rb
+++ lib/gamefic-standard/actions/quit.rb
@@ -6,13 +6,8 @@ module Gamefic
       module Quit
         extend Gamefic::Scriptable
 
-        yes_or_no :standard_confirm_quit do |actor, props|
-          actor.cue :default_conclusion if props.yes?
-        end
-
         meta :quit do |actor|
-          actor.tell 'Are you sure you want to quit?'
-          actor.cue :standard_confirm_quit
+          actor.run Clips::ConfirmQuit
         end
       end
     end
