67446fce64d30d7d6aa487d524551dfad80e42dc
Fred Snyder <fsnyder@castwide.com>
2024-10-16 21:09:58 -0400

    Bound default_conclusion

--- lib/gamefic-standard/actions/quit.rb
+++ lib/gamefic-standard/actions/quit.rb
@@ -6,18 +6,17 @@ module Gamefic
       module Quit
         extend Gamefic::Scriptable
 
-        ConfirmQuit = yes_or_no :confirm_quit do |scene|
+        ConfirmQuit = yes_or_no do |scene|
           scene.on_start do |actor|
             actor.tell 'Are you sure you want to quit?'
           end
           scene.on_finish do |actor, props|
-            # @todo default_conclusion doesn't work here
-            actor.cue Gamefic::Scene::Conclusion if props.yes?
+            actor.cue default_conclusion if props.yes?
           end
         end
 
         meta :quit do |actor|
-          actor.cue :confirm_quit
+          actor.cue ConfirmQuit
         end
       end
     end
