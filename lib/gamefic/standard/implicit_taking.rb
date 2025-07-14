de8d7d5280d65f9af574c5289f538e43dc2ea61b
Fred Snyder <fsnyder@castwide.com>
2025-07-14 10:33:35 -0400

    Documentation

--- lib/gamefic/standard/implicit_taking.rb
+++ lib/gamefic/standard/implicit_taking.rb
@@ -2,7 +2,25 @@
 
 module Gamefic
   module Standard
+    # A mixin that provides a #have_or_take method to automate possession.
+    # Authors can use it to ensure that an actor is holding an entity as a
+    # prerequisite to doing something with it.
+    #
     module ImplicitTaking
+      # True if the actor is already holding the entity or successfully takes
+      # possession of it.
+      #
+      # @example Actor needs to be holding the item to use it
+      #   respond :use, Item do |actor, item|
+      #     if actor.have_or_take(item)
+      #       actor.tell "You're using #{the item}."
+      #     else
+      #       actor.tell "You can't use #{the item}."
+      #     end
+      #   end
+      #
+      # @param entity [Gamefic::Entity]
+      # @return [Boolean]
       def have_or_take(entity)
         execute(:take, entity) unless possessions.include?(entity)
 
@@ -12,6 +30,8 @@ module Gamefic
 
       private
 
+      # @param from [Array<Gamefic::Entity>]
+      # @return [Array<Gamefic::Entity>]
       def possessions(from = children)
         return [] if from.empty?
 
